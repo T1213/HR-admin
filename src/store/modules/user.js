4baa007621f4b2cca7d5bdfbac8397afc8e09072
T1213 <1711917202@qq.com>
2022-07-13 17:22:02 +0800

    完成了登录页面

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,23 @@
-const state = {}
-const mutations = {}
-const actions = {}
+import { login } from '@/api/user'
+const state = {
+  token: null
+}
+const mutations = {
+  setToken (state, payload) {
+    state.token = payload
+  }
+}
+const actions = {
+  async login ({ commit }, data) {
+    try {
+      const res = await login(data)
+      console.log(res)
+      commit.dispatch('settoken', res.data.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
 export default {
   namespaced: true,
   state,
